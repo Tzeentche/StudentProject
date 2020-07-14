f0e3d4d63acc8b6d5fb03a287c1acbd240e0cea0
Tzeentche <ilyasugako87@gmail.com>
2020-07-14 23:21:56 +0300

    Refactored ExampleClass class.

--- src/ExampleClass.java
+++ src/ExampleClass.java
@@ -1,3 +1,34 @@
 public class ExampleClass  {
 
+    void method() {
+        Car car = new Ferrary();
+        car.Run();
+        car.Stop();
+    }
+}
+
+abstract class Car {
+    int speed;
+    abstract void Run();
+    void Stop() {
+        speed = 0;
+    }
+}
+
+class Ferrary extends Car {
+
+    @Override
+    void Run() {
+        speed = 25;
+    }
+}
+
+abstract class Ball extends ExampleClass {
+
+}
+
+final class MyFinalClass {
+    void someLogic() {
+
+    }
 }
