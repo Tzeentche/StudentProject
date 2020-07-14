f732a4cc90d8f704af1304205c4e80f726042c4c
Tzeentche <ilyasugako87@gmail.com>
2020-07-14 23:28:56 +0300

    Refactored SomeInterface interface.

--- src/SomeInterface.java
+++ src/SomeInterface.java
@@ -15,7 +15,7 @@ class Example implements SomeInterface {
 
     @Override
     public void addTwoDigits(int one, int two) {
-        SOME_VARIABLE = 5;
+        
     }
 
     public void someMethod() {
