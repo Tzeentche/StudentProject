6d6709a75e6fd64ebf15c3d558bc3475c7855f72
Tzeentche <ilyasugako87@gmail.com>
2020-07-14 23:57:57 +0300

    Refactored Methods class.

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,20 @@
 public class Methods {
+    static int i = 5;
+    static void method(int i, float f, boolean b, Object o, int... j) {
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(o);
 
-    
+        for(int k : j) {
+            System.out.println(k);
+        }
+    }
+}
+
+class otherExample extends Methods {
+
+    public static void main(String[] args) {
+        Methods.method(5, 4.32464f, true, new Object(), 5, 46, 35, 36, 46, 534, 35, 63, 456, 53, 47,34);
+    }
 }
