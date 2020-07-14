66f20c2783ed29cd1384fd02bbe0fe09f88838c6
Tzeentche <ilyasugako87@gmail.com>
2020-07-14 23:26:49 +0300

    Refactored SomeInterface interface.

--- src/SomeInterface.java
+++ src/SomeInterface.java
@@ -1,4 +1,28 @@
-public interface SomeInterface {
+interface SomeInterface extends IOtherInterface {
+    int SOME_VARIABLE = 5;
+    abstract void addTwoDigits(int one, int two);
+}
 
-    
+interface IOtherInterface {
+    void someMethod();
+}
+
+interface Bancable {
+    void bounce();
+}
+
+class Example implements SomeInterface {
+
+    @Override
+    public void addTwoDigits(int one, int two) {
+        SOME_VARIABLE = 5;
+    }
+
+    public void someMethod() {
+
+    }
+
+    public void bounce() {
+
+    }
 }
