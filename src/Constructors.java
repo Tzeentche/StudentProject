ba2dec1fd528a41df52051c01536443534fafa6e
Tzeentche <ilyasugako87@gmail.com>
2020-07-15 23:08:48 +0300

    Refactored Constructors class.

--- src/Constructors.java
+++ src/Constructors.java
@@ -1,2 +1,22 @@
 public class Constructors {
+
+    int i;
+
+    Constructors(String hello, int i) {
+        this.i = i;
+        System.out.println("Ron");
+        System.out.println(hello);
+    }
+
+    Constructors() {
+        this.i = i;
+        System.out.println("Run");
+    }
+}
+
+class Run {
+    public static void main(String[] args) {
+        Constructors constructors = new Constructors();  
+        System.out.println(constructors.i);
+    }
 }
