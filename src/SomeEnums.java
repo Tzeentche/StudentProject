d78794a821e1022c43b26e4cbacfea7a18717ba8
Tzeentche <ilyasugako87@gmail.com>
2020-07-15 23:22:52 +0300

    Refactored SomeEnums class.

--- src/SomeEnums.java
+++ src/SomeEnums.java
@@ -1,3 +1,24 @@
 public class SomeEnums {
+    
+    enum CoffeSize {SMALL(100), MEDIUM(200), BIG(300)
 
+        String coffeClass;
+        int milliliters;
+        CoffeSize(int milliliters) {
+            this.milliliters = milliliters;
+        }
+
+        int getMilliliters() {
+            return milliliters;
+        }
+        String getCoffeclass() {
+            return coffeClass;
+        }
+        }
+
+    public static void main(String[] args) {
+        CoffeSize coffeSize = CoffeSize.BIG;
+        System.out.println(coffeSize);
+        System.out.println(coffeSize.getMilliliters());
+    }
 }
