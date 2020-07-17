27b595b584ce2d9f67eed1382966d7a04ef6f3cc
Tzeentche <ilyasugako87@gmail.com>
2020-07-17 23:56:19 +0300

    Refactored Overridings class.

--- src/Overridings.java
+++ src/Overridings.java
@@ -1,2 +1,24 @@
+
+class Parent {
+    int method() {
+        return 1;
+    }
+}
+
+class Child extends Parent {
+    @Override
+    int method() {
+        return 2;
+    }
+}
+
 public class Overridings {
+
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        Child child = new Child();
+
+        System.out.println(parent.method());
+        System.out.println(child.method());
+    }
 }
