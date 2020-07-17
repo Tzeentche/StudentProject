03b5ec5f4c8b329ceb77e6d6abb4ea8bab11a6d3
Tzeentche <ilyasugako87@gmail.com>
2020-07-17 23:45:05 +0300

    Refactored Overloads class.

--- src/Overloads.java
+++ src/Overloads.java
@@ -1,2 +1,27 @@
+import java.io.IOException;
+
 public class Overloads {
+
+    int addTwoDigits(int a, int b) throws IOException {
+        return a+b;
+    }
+
+    double addTwoDigits(double a, double b) throws RuntimeException {
+        return a + b;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Overloads overloads = new Overloads();
+        System.out.println(overloads.addTwoDigits(5, 6));
+        System.out.println(overloads.addTwoDigits(5.4, 3.0));
+        System.out.println(overloads.addTwoDigits(3, 1));
+    }
+}
+
+class OverloadChild extends Overloads {
+
+    void addTwoDigit(int i) {
+        System.out.println("Overload method");
+    }
 }
