3588f78f8b4c988fa76ecfd029c65e53e12c6e1e
Tzeentche <ilyasugako87@gmail.com>
2020-07-18 23:55:25 +0300

    Refactored Statics class.

--- src/Statics.java
+++ src/Statics.java
@@ -1,2 +1,29 @@
 public class Statics {
+
+    static int i = 5;
+    int j;
+
+    static {
+        System.out.println("Static initializer");
+    }
+    {
+        System.out.println("Initializer");
+    }
+
+    Statics() {
+        System.out.println("Constructor");
+    }
+
+    static void staticMethod() {
+        System.out.println("Static method");
+    }
+
+    public static void main(String[] args) {
+        Statics statics = new Statics();
+        Statics statics2 = new Statics();
+        statics.j = 1;
+        statics2.j = 2;
+        System.out.println(statics.j);
+        System.out.println(statics2.j);
+    }
 }
