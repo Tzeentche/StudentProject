0b543578754e86ef362112591c801b130685f372
Tzeentche <ilyasugako87@gmail.com>
2020-07-19 12:43:52 +0300

    Refactored Initialisations class.

--- src/Initialistions.java
+++ src/Initialistions.java
@@ -1,3 +1,24 @@
-public class Initialistions {
-    
+class Parente {
+    Parente() {
+        System.out.println("Parent constructor");
+    }
+}
+
+public class Initialistions extends Parente {
+
+    static {
+        System.out.println("Static init block");
+    }
+    {
+        System.out.println("Init block");
+    }
+    Initialistions() {
+        super();
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+        new Initialistions();
+
+    }
 }
