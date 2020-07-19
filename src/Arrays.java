349413c4a2f31afacd65d45b38a396ad2e50be28
Tzeentche <ilyasugako87@gmail.com>
2020-07-19 12:59:43 +0300

    Refactored Arrays class.

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,4 +3,9 @@ public class Arrays {
     int[] arr = {5, 4, 2};
     boolean[] booleanArray = {true, false};
     double[] doubles = {5.234234, 5.235235};
+
+    public static void main(String[] args) {
+        long[] l = new long[]{1, 5, 7, 9, 11, 13};
+        int[][] j = {{1, 3,},{2, 17,}};
+    }
 }
