793982221f0c8cf06528edfe1cc4a10b504db3c6
Tzeentche <ilyasugako87@gmail.com>
2020-07-21 23:32:50 +0300

    Refactored Whiles class.

--- src/Whiles.java
+++ src/Whiles.java
@@ -1,4 +1,15 @@
 public class Whiles {
 
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int i = 0;
+        while(i < arr.length) {
+            System.out.println(i);
+            i++;
+
+        } do {
+            System.out.println(arr[i]);
+            i++;
+        } while (i < arr.length);
+    }
 }
