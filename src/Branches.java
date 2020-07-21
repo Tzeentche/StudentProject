53e67f2df6f40f136ad4307e3c07f193e613f052
Tzeentche <ilyasugako87@gmail.com>
2020-07-21 23:20:08 +0300

    Refactored Branches class.

--- src/Branches.java
+++ src/Branches.java
@@ -1,2 +1,16 @@
 public class Branches {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 6;
+        boolean bl = a == b;
+
+        if(bl == true) {
+
+            System.out.println(bl);
+        } else {
+            System.out.println(a != b);
+        }
+    }
 }
