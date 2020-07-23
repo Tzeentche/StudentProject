1197146e1a8913190be629dc1d654aae071bd8e5
Tzeentche <ilyasugako87@gmail.com>
2020-07-23 23:29:16 +0300

    Refactored Forss class.

--- src/Forss.java
+++ src/Forss.java
@@ -6,5 +6,9 @@ public class Forss {
         for(int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+        for(int i : arr) {
+            System.out.println(i);
+        }
     }
 }
