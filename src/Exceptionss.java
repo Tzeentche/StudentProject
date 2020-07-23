09ce7a06d977611c11d87fd789f532a7363c94b9
Tzeentche <ilyasugako87@gmail.com>
2020-07-23 23:32:09 +0300

    Refactored Exceptionss class.

--- src/Exceptionss.java
+++ src/Exceptionss.java
@@ -1,2 +1,14 @@
 public class Exceptionss {
+
+    public static void main(String[] args) {
+        new Exceptionss().someMethod();
+    }
+
+    void someMethod() {
+        try {
+            Object o = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
