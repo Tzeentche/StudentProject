9c2b211456bd0ce550a8505727154b9b8f34da79
Tzeentche <ilyasugako87@gmail.com>
2020-07-25 23:42:51 +0300

    Refactored Stringes class.

--- src/Stringes.java
+++ src/Stringes.java
@@ -1,2 +1,22 @@
 public class Stringes {
+
+    public static void main(String[] args) {
+
+        String s = new String("abc");
+        String s2 = s.concat("def");
+        System.out.println(s);
+        System.out.println(s2);
+
+        StringBuilder strBui = new StringBuilder();
+        strBui.append("ghi");
+        strBui.insert(0, "");
+        System.out.println(strBui);
+
+        StringBuffer strBuf = new StringBuffer();
+        strBuf.append("jkl");
+        strBuf.insert(0, "");
+        System.out.println(strBuf);
+    }
+
+
 }
