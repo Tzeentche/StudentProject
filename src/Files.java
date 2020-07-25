abe3d4119989e62bc024bc64994b938c9fd642cb
Tzeentche <ilyasugako87@gmail.com>
2020-07-25 23:50:18 +0300

    Refactored Files class.

--- src/Files.java
+++ src/Files.java
@@ -1,2 +1,35 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Files {
+
+    public static void main(String[] args) {
+
+        File file = new File("tmp.txt");
+
+        if(file.exists()) {
+            file.mkdir();
+            System.out.println("exists");
+        }
+
+        if(file.isDirectory()) {
+            System.out.println("dir");
+        }
+
+        if(file.isFile()) {
+            System.out.println("file");
+        }
+
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("str1\n");
+        fileWriter.write("str2\n");
+        fileWriter.flush();
+        fileWriter.close();
+
+        FileReader fileReader = new FileReader(file);
+        char[] someChars = new char[20];
+        fileReader.read(someChars);
+        System.out.println(someChars);
+    }
 }
