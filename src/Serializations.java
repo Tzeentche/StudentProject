ddd777de16517c42c5c2014a79a375e056d98397
Tzeentche <ilyasugako87@gmail.com>
2020-07-26 14:52:04 +0300

    Refactored Serializations class.

--- src/Serializations.java
+++ src/Serializations.java
@@ -1,2 +1,36 @@
+import java.io.*;
+
 public class Serializations {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        user.lifeLevel = 55;
+
+        Sword sword = new Sword();
+        sword.level = 5;
+        user.sword = sword;
+
+        FileOutputStream fileOutputStream = new FileOutputStream("tempfile");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(user);
+        objectOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("tempFile");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        User newUser = (User) objectInputStream.readObject();
+        objectInputStream.close();
+
+        System.out.println(newUser.lifeLevel);
+        System.out.println(newUser.sword.level);
+    }
+}
+
+class User implements Serializable {
+
+    int lifeLevel;
+    Sword sword;
+}
+
+class Sword implements Serializable {
+    int level;
 }
