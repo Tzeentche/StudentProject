d0d940a5ba2b2799c8c0f40f8e03f86c96fb7a52
Tzeentche <ilyasugako87@gmail.com>
2020-07-28 23:24:39 +0300

    Refactored Dates class.

--- src/Dates.java
+++ src/Dates.java
@@ -1,6 +1,28 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Dates {
 
-    
+    public static void main(String[] args) throws ParseException {
+        Date date = new Date();
+        System.out.println(date);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.WEEK_OF_MONTH, 0);
+        System.out.println(calendar);
+        System.out.println(calendar.getTime());
+
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+        System.out.println(dateFormat.format(calendar.getTime()));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy");
+        System.out.println(simpleDateFormat.format(calendar.getTime()));
+
+        Date newDate = simpleDateFormat.parse("09/01/1987");
+        System.out.println(newDate);
+    }
 }
