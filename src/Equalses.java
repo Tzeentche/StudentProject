414bc5c6daad89e0c3357e4fc906d1f2695ba1e1
Tzeentche <ilyasugako87@gmail.com>
2020-07-29 23:53:49 +0300

    Refactored Equalses class.

--- src/Equalses.java
+++ src/Equalses.java
@@ -12,7 +12,11 @@ public class Equalses {
         System.out.println(equalses + "");
 
         Map<String, Equalses> map = new HashMap<>();
-        
+
+        Book book = new Book();
+        book.author = "Carnegy";
+        Map<String, Book> library = new HashMap<>();
+        library.put("How to get friends", book);
     }
 
     @Override
