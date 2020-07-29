1613efccc97a7cf0fcf63d5ec14dd635b212d091
Tzeentche <ilyasugako87@gmail.com>
2020-07-29 23:53:37 +0300

    Refactored Equalses class.

--- src/Equalses.java
+++ src/Equalses.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Equalses {
+
+    int value;
+
+    public static void main(String[] args) {
+
+        Equalses equalses = new Equalses();
+        equalses.value = 5;
+        System.out.println(equalses + "");
+
+        Map<String, Equalses> map = new HashMap<>();
+        
+    }
+
+    @Override
+    public String toString() {
+        return value + "value";
+    }
+}
+
+class Book {
+    String author;
 }
