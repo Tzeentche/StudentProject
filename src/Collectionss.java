0aab9c461b5abc9e9f6202b6ecda76251a2c80c2
Tzeentche <ilyasugako87@gmail.com>
2020-07-30 23:38:17 +0300

    Refactored Collectionss class.

--- src/Collectionss.java
+++ src/Collectionss.java
@@ -12,13 +12,30 @@ public class Collectionss {
             System.out.println(collection1.get(i));
         }
 
+        Queue collection3 = new PriorityQueue();
+        collection3.offer("1");
+        collection3.offer("2");
+        collection3.offer("3");
+        collection3.offer("4");
+        Iterator iterator = collection3.iterator();
+        for(int i = 0; i < collection3.size(); i++) {
+            System.out.println(collection3.poll());
+        }
+
+        while(iterator.hasNext()) {
+            System.out.println(collection3.poll());
+
+        }
+
+        System.out.println("Num of elements " + collection3.size());
+
         Collection collection = new ArrayList<>();
         collection.add("1");
         collection.add("2");
         collection.add("3");
         collection.remove("2");
 
-        Iterator iterator = collection.iterator();
+        Iterator iterator2 = collection.iterator();
 
         for (Object o : collection) {
             System.out.println(o);
