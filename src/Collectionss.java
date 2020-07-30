c1905a246e2b0662061dfe494117fb4e4035d526
Tzeentche <ilyasugako87@gmail.com>
2020-07-30 23:22:19 +0300

    Refactored Collectionss class.

--- src/Collectionss.java
+++ src/Collectionss.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class Collectionss {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[15];
+        Collection collection = new ArrayList<>();
+        collection.add("1");
+        collection.add("2");
+        collection.add("3");
+        collection.remove("2");
+
+        Iterator iterator = collection.iterator();
+
+        for (Object o : collection) {
+            System.out.println(o);
+        }
+    }
 }
