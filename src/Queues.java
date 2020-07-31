ad2b76bd9feaafafb63d12d7d563eb7363a271b7
Tzeentche <ilyasugako87@gmail.com>
2020-08-01 00:02:45 +0300

    Refactored Queues class.

--- src/Queues.java
+++ src/Queues.java
@@ -1,4 +1,20 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Queues {
 
-    
+    public static void main(String[] args) {
+
+        Queue queue = new LinkedList<>();
+        Queue queue2 = new PriorityQueue<>();
+
+        queue.offer("1");
+        queue.offer(2);
+        queue.offer("3");
+
+        while (queue.size() > 0) {
+            System.out.println(queue.poll());
+        }
+    }
 }
