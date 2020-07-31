1dda5309d875cb029b7aa0bf57a4ed5aba245d30
Tzeentche <ilyasugako87@gmail.com>
2020-08-01 00:00:07 +0300

    Create Queues class.

--- src/Lists.java
+++ src/Lists.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
@@ -7,7 +8,7 @@ public class Lists {
     public static void main(String[] args) {
         List list = new ArrayList();
         List list2 = new Vector();
-        List list = new ArrayList();
+        List list3 = new LinkedList();
         list.add(1);
         list.add(2);
         list.add(3);
