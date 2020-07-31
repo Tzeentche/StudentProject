732ea3ddbc4102910d44ea203eb62b2b2693200e
Tzeentche <ilyasugako87@gmail.com>
2020-07-31 23:47:32 +0300

    Refactored List class.

--- src/Lists.java
+++ src/Lists.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 public class Lists {
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        List list2 = new Vector();
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }
