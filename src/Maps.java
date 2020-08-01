e8fdea4b94c5b7104104476d005773f8f6657837
Tzeentche <ilyasugako87@gmail.com>
2020-08-01 23:24:47 +0300

    Refactored Maps class.

--- src/Maps.java
+++ src/Maps.java
@@ -1,2 +1,18 @@
+import java.util.*;
+
 public class Maps {
+
+    public static void main(String[] args) {
+
+        Map map = new HashMap();
+        Map map2 = new Hashtable();
+        Map map3 = new LinkedHashMap();
+        Map map4 = new TreeMap();
+
+        map.put("1", "One");
+        map.put("3", "Three");
+        map.put("2", "two");
+
+        System.out.println(map.get(3));
+    }
 }
