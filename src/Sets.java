dded47a6d927cbdc3891db7f0227a6021b13e8db
Tzeentche <ilyasugako87@gmail.com>
2020-08-02 23:43:46 +0300

    Refactored Sets class.

--- src/Sets.java
+++ src/Sets.java
@@ -1,2 +1,24 @@
+import java.io.ObjectInputStream;
+import java.util.*;
+
 public class Sets {
+
+    public static void main(String[] args) {
+
+        Set set2 = new HashSet();
+        Set set = new LinkedHashSet();
+        Set set3 = new TreeSet();
+
+        set.add("6");
+        set.add("2");
+        set.add("3");
+        set.add("1");
+        set.add("1");
+        set.add("2");
+        set.add("1");
+
+        for(Object o : set) {
+            System.out.println(o);
+        }
+    }
 }
