cca6628c1d6641cc8c746a12f55b3d3ba6212fc2
Tzeentche <ilyasugako87@gmail.com>
2020-08-02 23:54:24 +0300

    Refactored Genericss class.

--- src/Genericss.java
+++ src/Genericss.java
@@ -1,2 +1,31 @@
-public class Genericss {
+import java.io.*;
+import java.util.*;
+
+class Cars extends Genericss implements Comparable, Serializable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
+
+public class Genericss <T extends Genericss & Comparable & Serializable> {
+
+    T var;
+
+    public static void main(String[] args) throws Exception {
+
+        Genericss<Cars> genericss = new Genericss<>();
+        genericss.var = new Cars();
+        System.out.println(genericss.var);;
+    }
+
+    T method(T type) {
+        type.someMethos();
+        return type;
+    }
+
+    void someMethos() {
+        System.out.println("Bla!");
+    }
 }
