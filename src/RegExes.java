12847487c9b8e0e1c34a99a074da08b1bc77b71e
Tzeentche <ilyasugako87@gmail.com>
2020-08-05 23:59:00 +0300

    Refactored RegExes class.

--- src/RegExes.java
+++ src/RegExes.java
@@ -1,7 +1,13 @@
+import java.util.regex.Pattern;
+
 public class RegExes {
 
     public static void main(String[] args) {
 
-        
+        String cardNumber = "2422333156849792";
+        String date = "03/12";
+        String CVV = "123";
+
+        Pattern cardNumberPattern = Pattern.compile()
     }
 }
