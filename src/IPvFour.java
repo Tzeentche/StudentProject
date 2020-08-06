706b344d7375e443680c6ff370e907de99a4887e
Tzeentche <ilyasugako87@gmail.com>
2020-08-06 23:52:34 +0300

    Refactored IPvFour class.

--- src/IPvFour.java
+++ src/IPvFour.java
@@ -1,2 +1,12 @@
+import java.util.regex.Pattern;
+
 public class IPvFour {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+        String IPv4 = "0.0.0.0";
+
+        Pattern pattern = Pattern.compile("");
+    }
 }
