1f3c8b0d86ffcb5b2d38748f0a85b4a7ef807f57
Tzeentche <ilyasugako87@gmail.com>
2020-08-06 23:57:43 +0300

    Refactored IPvFour class.

--- src/IPvFour.java
+++ src/IPvFour.java
@@ -10,5 +10,15 @@ public class IPvFour {
 
         Pattern pattern = Pattern.compile("^(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])$");
         Matcher matcher = pattern.matcher(IPv4);
+
+        while (matcher.find()) {
+            counter++;
+
+            System.out.println(("Match found '" + IPv4.substring(matcher.start(), matcher.end()) +
+                    "'. Starting at index " + matcher.start() +
+                    " and ending at index " + matcher.end()));
+        }
+
+        System.out.println("Matches found");
     }
 }
