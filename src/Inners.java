b9789b4ea35fc707b83bd27b2918c9fc9a3fdaee
Tzeentche <ilyasugako87@gmail.com>
2020-08-08 22:57:47 +0300

    Create Refactored class.

--- src/Inners.java
+++ src/Inners.java
@@ -1,2 +1,35 @@
 public class Inners {
+
+    private int i = 5;
+    Inner inner = new Inner();
+
+    public static void main(String[] args) {
+        Inners inners = new Inners();
+        inners.method();
+
+        Inner inner = inners.new Inner();
+        inner.method2();
+
+    }
+
+    void method() {
+        Inner inner = new Inner();
+        inner.method2();
+        System.out.println(inner.k);
+    }
+
+    class Inner {
+        private int k = 6;
+        void method2() {
+            System.out.println(i);
+        }
+    }
+}
+
+class Second {
+
+    void method() {
+        Inners.Inner inner = new Inners().new Inner();
+        inner.method2();
+    }
 }
